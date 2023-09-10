81c09f2fcbcea5a80735bc6612c6319f2d66e35f
devJomar217 <84326567+devJomar217@users.noreply.github.com>
2023-09-10 15:48:09 +0800

    New changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'common/session.php';
-include 'common/common.php';
+// include 'common/common.php';
 ?>
 
 <html lang="en">
@@ -8,6 +8,7 @@ include 'common/common.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Code Connect</title>
+    <link rel="icon" href="./resource/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body style="background-color: rgb(245, 245, 245)">
@@ -20,7 +21,7 @@ include 'common/common.php';
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="./common/common.js"></script>
 <script>
-    // loadLoginForm("template", "./component/login/login.html");
+    // loadLoginForm("template", "./public/login/login.html");
 </script>
-<script src="common/db.js"></script>
+<!-- <script src="common/db.js"></script> -->
 </html>
---
+++ public/login/index.php
@@ -0,0 +1,68 @@
+<?php
+include '../../common/session.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code Connect</title>
+    <link rel="stylesheet" href="./../../common/vendor/bootstrap/bootstrap.min.css">
+    <link rel="icon" href="./../../resource/favicon.ico">
+</head>
+
+<body style="background-color: rgb(245, 245, 245)">
+
+    <section class="login">
+        <div class="py-5 h-100 " style="background-color: hsl(0, 0%, 96%)">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center d-none d-sm-block">
+                        <div class="row">
+                            <div class="col-2"></div>
+                            <div class="col-8 text-center">
+                                <img class="img-fluid m-1" style="width: 300px" src="./../../resource/logo-bsit.png" alt="" srcset="">
+                                <img class="img-fluid" src="./../../resource/logo-codeconnect-colored.png" alt="" srcset="">
+                                <p style="color: hsl(217, 10%, 50.8%)">
+                                    <h4><b><i>“Learning by Asking”</i></b></h4>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col mb-5 mb-lg-0">
+                        <div class="card">
+                            <div class="card-body py-5 px-md-5 " id="login-form">
+                                <div id="card-form"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Jumbotron -->
+    </section>
+
+    <!-- Section: Design Block -->
+</body>
+
+<script src="./../../common/vendor/jquery/jquery.min.js"></script>
+<script src="./../../common/vendor/jquery/jquery.cookie.js"></script>
+<script src="./../../common/vendor/jquery/aes.js"></script>
+<script src="./../../common/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./../../common/common.js"></script>
+<!-- <script src="common/db.js"></script> -->
+<script>
+    $(document).ready(function() {
+        const queryString = window.location.search;
+        console.log(queryString);
+        const urlParams = new URLSearchParams(queryString);
+        console.log(urlParams);
+
+        $("#card-form").load('./form-login.html');
+    });
+</script>
+
+</html>
